5f94e0e75ef7b771d50a0a9d1ac3ea635e428e90
Kazan <kazanuge77@gmail.com>
2025-06-12 08:05:45 -0600

    featu: Agregu una funcion para capturar datos perso del alumno

--- estudiantes.cpp
+++ estudiantes.cpp
@@ -26,6 +26,36 @@ int pedirEnteroPositivo(const string& mensaje) {
     }
 }
 
+struct Alumno {
+    string nombre, apellido1, apellido2;
+    int ciclo, cedula;
+    vector <float> notas;
+    float promedio;
+};
+// Estos espara capturar los datos y el de arriba usted sabe para que es
+
+Alumno capturaAlumno(int numero){
+    Alumno a;
+    cout << "\n==============================" << endl;
+    cout << "  Ingreso de datos - Alumno #" << numero << endl;
+    cout << "==============================" << endl;
+
+    cout << "Ingrese primer nombre";
+    cin.ignore();
+    getline(cin, a.nombre);
+
+    cout << "Ingrese primer apellido: ";
+    getline(cin, a.apellido1);
+
+    cout << "Ingrese segundo apellido: ";
+    getline(cin, a.apellido2);
+
+    a.ciclo=pedirEnteroPositivo("Ingrese ciclo de estudio: ");
+    a.cedula=pedirEnteroPositivo("Ingrese numero de cedula");
+
+    return a;
+}
+
 int main (){
     int cantidadAlumnos = pedirEnteroPositivo ("Cuantos alumnos desea evaluar? ");
 
@@ -33,7 +63,18 @@ int main (){
     // Darian usted puede ir implementando la estructura Alumno y el Vector dinamico de los Alumnos
     // A su vezpuedes definir la funcion capturaAlumnos() para que nos ordenemos mejor
 
-    cout << "Cantidad de alumnos a evaluar: " << cantidadAlumnos << endl;
+    vector<Alumno> alumnos;
+
+    for(int i=0; i< cantidadAlumnos; i++){
+        Alumno nuevoAlumno =capturaAlumno(i + 1);
+        alumnos.push_back(nuevoAlumno);
+
+        //Te encvargo capturar las 5 notas del alumno y calcular el promedio de estas
+        //Use Validacionees para cada nota: entre 0.0 y 100.0
+        //Tambien guarda las notas en nuevoAlumno.notas y calcule el promedio en nuevoAlumno.promedio
+        // Crea una funcion calcularPromedio(), [Nos ayudara,HAAAAAAAAAHAHAHAH]
+    }
+    cout << "\nCaptura de datos personales finalizada" << endl;
 
     return 0;
 }
